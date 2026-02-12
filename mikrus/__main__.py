fa047674c06992e0610aaf6cb571a510373b558c
Jakub Suchenek <jakub.suchenek.25@gmail.com>
2026-02-13 00:00:32 +0100

    Add more functions to main

--- mikrus/__main__.py
+++ mikrus/__main__.py
@@ -13,5 +13,12 @@ if __name__ == '__main__':
     try:
         info = server.info()
         print(f'Server: {info.server_name} (ID: {info.server_id})')
+        server_list = server.servers()
+        print(f'Server list: {', '.join(map(str, server_list.servers)) if server_list else 'None'}')
+        logs = server.logs_list()
+        print(f'Server logs: {str(logs) if logs else 'None'}')
+        print(f'Last log: {server.log_by_id(logs[0].id) if logs else 'None'}')
+        databases = server.databases()
+        print(f'Databases: {str(databases) if databases else 'None'}')
     except HTTPError as e:
         print(f'Error {e.errno}: {e.strerror}')
